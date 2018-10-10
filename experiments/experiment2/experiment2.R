7d4881b00617d373b761ff61a017caeacfcbd8a1
James Browne <jbrowne6@jhu.edu>
2018-10-10 13:48:09 -0400

    still working on fixing the depthAndStripe experiment2.

--- experiments/experiment2/experiment2.R
+++ experiments/experiment2/experiment2.R
@@ -10,7 +10,7 @@ library(plyr)
 data_summary <- function(data, varname, groupnames){
       require(plyr)
   summary_func <- function(x, col){
-          c(mean = mean(x[[col]], na.rm=TRUE),
+          c(mean = median(x[[col]], na.rm=TRUE),
                   sd = sd(x[[col]], na.rm=TRUE))
     }
     data_sum<-ddply(data, groupnames, .fun=summary_func,
@@ -20,35 +20,41 @@ data_summary <- function(data, varname, groupnames){
 }
 
 
-mydata <- read.csv(file="experiment1.csv", header=FALSE)
+mydata <- read.csv(file="experiment2.csv", header=FALSE)
 
-mydata <- data_summary(mydata,varname="V6",groupnames=c("V2","V3","V4","V5"))
-mydata$V4 <- as.factor(mydata$V4)
-mydata$V2 <- as.factor(mydata$V2)
-mydata$V3 <- as.factor(mydata$V3)
-mydata$V5 <- as.numeric(mydata$V5)
-mydata$V6 <- mydata$V6/1000000
+z <- mydata$V3 == " MNIST"
+mydata[z,]$V7 <- mydata[z,]$V7/10000
+z <- mydata$V3 == " Higgs"
+mydata[z,]$V7 <- mydata[z,]$V7/25000
+z <- mydata$V3 == " Allstate"
+mydata[z,]$V7 <- mydata[z,]$V7/50000
 
-sCore <- mydata$V4==" SingleCore"
-mydata <- rbind(mydata, mydata[sCore,])
-mydata$V5[(length(mydata$V5)-sum(sCore)+1):length(mydata$V5)] <- max(mydata$V5)
+mydata$V8 <- as.factor(mydata$V8)
 
-leg <- theme(legend.text = element_text(size = 12), legend.title=element_text(size = 12), plot.title = element_text(size = 16,  face="bold"), axis.title.x = element_text(size=15), axis.text.x = element_text(size=15), axis.title.y = element_text(size=15), axis.text.y = element_text(size=15))
+mydata <- data_summary(mydata, varname="V7",groupnames=c("V3", "V5", "V8"))
+#mydata <- mydata[mydata$V6!=6,]
 
-p <- ggplot(mydata, aes(x=V5, y=V6, group=V2, color=V2)) + geom_line()
+leg <- theme(legend.text = element_text(size = 13), legend.title=element_text(size = 13), plot.title = element_text(size = 16,  face="bold"), axis.title.x = element_text(size=13), axis.text.x = element_text(size=13), axis.title.y = element_text(size=13), axis.text.y = element_text(size=13), strip.text.x = element_text(size=13), strip.text.y = element_text(size=13))
+
+p <- ggplot(mydata, aes(x=V5, y=V7, group=V8, color=V8)) + geom_line(size=1)
   
+
+
 p <- p + scale_fill_brewer(palette="Paired") + theme_minimal()
-p <- p + guides(fill=guide_legend(title="Technique"))
-p <- p + labs(x = "Cores Used", y = "Test Set Prediction Time (s)")
-p <- p + scale_colour_manual(values=c("red2", "blue2", "orange2", "green2", "purple2", "cyan2"), 
-														 name="Encoding", 
-														 breaks=c(" BF"," DF"," DF-"," Stat"," Bin"," Bin+"), 
-														 labels=c("BF","DF","DF-","Stat","Bin","Bin+"))
+#p <- p + scale_colour_manual(name="Depth")
+
+#p <- p + scale_colour_manual(name="Number of Threads")
+p <- p + theme(legend.position="bottom")
+p <- p + labs(x = "Trees Per Bin", y =expression(paste("Mean Prediction Time per Observation (", mu, "s)")))
 p <- p + leg
-p <- p + facet_grid(V4 ~ V3)
-
-
-png(file="experiment1.png")
+p <- p + scale_x_continuous(trans='log2')
+#p <- p + scale_y_continuous(trans='log2')
+p <- p + facet_grid(. ~ V3)
+p <- p + theme(strip.background = element_rect(fill="grey95"))
+p <- p + theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text.y = element_text(angle=90, hjust=1))
+#+ guides(colour = guide_legend(nrow = 1))
+p <- p + labs(color="Number of Threads:")
+png(file="depthAndStripe.png")
 print(p)
 dev.off()
 
--- experiments/experiment2/experiment2.cpp
+++ experiments/experiment2/experiment2.cpp
@@ -11,10 +11,7 @@
 #include <thread>
 #include "inferenceSamples.h"
 #include "improv1.h"
-//#include "improv3.h"
-//#include "improv7.h"
 #include "improv4.h"
-//#include "improv5.h"
 #include "improv6.h"
 #include "improv2.h"
 #include "improv8.h"
@@ -27,27 +24,16 @@ inline bool doesExist(const std::string& name) {
 
 int main(int argc, char* argv[]) {
 
-	int useHT;
-	useHT=atoi(argv[1]);
-
-	int maxCores;
-
-	maxCores=4;
 	std::ofstream outfile;
-	if(useHT==1){
-		outfile.open("experiments/experiment2/experiment2HT.csv");
-	}else{
-		outfile.open("experiments/experiment2/experiment2noHT.csv");
-	}
-
+	outfile.open("experiments/experiment2/experiment2.csv");
 
 	auto start_time = std::chrono::high_resolution_clock::now();
 	auto stop_time = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double, std::micro> diffMilli;
 
 	float percentRight = 0;
-	int numTimes = 2;
-
+	int numTimes = 10;
+	int numDepth = 5;
 
 	if(doesExist("res/higgs/forest.csv") && true){//begin higgs test namespace
 		const std::string forestFileName1 = "res/higgs/forest.csv";
@@ -56,129 +42,130 @@ int main(int argc, char* argv[]) {
 		inferenceSamples travs(traversalFileName1);
 		inferenceSamples observations(testFileName1);
 
-		{//start improv8 namespace
-			//////improv8/////////
-			std::cout << "\n\n\n"; 
-			std::cout << "running Higgs improv8 Test"; 
-
-			std::cout << "MultiTree\n"; 
-			for(int numCores = 1; numCores <= maxCores; numCores *= 2){	
-				std::cout << "MultiTree, numCores=" << numCores << "\n"; 
-				int treesPerBin = 2048/numCores;
-				improv8 tester(forestFileName1,1,travs, treesPerBin, 3);
-				tester.printForest();
-				printf("size of a node is %d\n",(int) sizeof(padNode));
-				printf("starting run\n");
-
-				for(int runNum = 0; runNum < numTimes; ++runNum){
-					start_time = std::chrono::high_resolution_clock::now();
-					tester.makePredictionsMultiTree(observations,numCores);
-					stop_time = std::chrono::high_resolution_clock::now();
-					diffMilli = stop_time - start_time;
-					outfile << std::fixed << "experiment1, Bin+, Higgs, MultiTree, " << numCores << ", " << diffMilli.count()<< std::endl;
-					if (observations.returnPercentRight() != percentRight){
-						std::cout << "percentRight does not match: " << percentRight << " : " << observations.returnPercentRight() << "\n";
-						//outfile << "percentRight does not match: " << percentRight << " : " << observations.returnPercentRight() << "\n";
-					}
-				}
-			}
-
-		}//end improv8 namespace
-
-	}//end higgs test namespace
-
-
-	////////////////////////////////////////////////////////////////////////////////////////
-	////////////////////////////////////////////////////////////////////////////////////////
-	////////////////////////////////////////////////////////////////////////////////////////
-
-	if (doesExist("res/mnist/forest.csv") && true){//begin mnist test namespace
-		const std::string forestFileName2 = "res/mnist/forest.csv";
-		const std::string testFileName2 = "res/mnist/testObservations.csv";
-		const std::string traversalFileName2 = "res/mnist/traversal.csv";
-
-		inferenceSamples travs(traversalFileName2);
-		inferenceSamples observations(testFileName2);
-
-
-		{//start improv8 namespace
-			//////improv8/////////
-			std::cout << "\n\n\n"; 
-			std::cout << "running MNIST improv8 Test"; 
-
-			std::cout << "MultiTree\n"; 
-			for(int numCores = 1; numCores <= maxCores; numCores *= 2){	
-				std::cout << "MultiTree, numCores=" << numCores << "\n"; 
-				std::cout << "MultiTree, numCores=" << numCores << "\n"; 
-				int treesPerBin = 2048/numCores;
-				improv8 tester(forestFileName2,1,travs, treesPerBin, 3);
-				tester.printForest();
-				printf("size of a node is %d\n",(int) sizeof(padNode));
-				printf("starting run\n");
-
-
-				for(int runNum = 0; runNum < numTimes; ++runNum){
-					start_time = std::chrono::high_resolution_clock::now();
-					tester.makePredictionsMultiTree(observations,numCores);
-					stop_time = std::chrono::high_resolution_clock::now();
-					diffMilli = stop_time - start_time;
-					outfile << std::fixed << "experiment1, Bin+, MNIST, MultiTree, " << numCores << ", " << diffMilli.count()<< std::endl;
-					if (observations.returnPercentRight() != percentRight){
-						std::cout << "percentRight does not match: " << percentRight << " : " << observations.returnPercentRight() << "\n";
-						//outfile << "percentRight does not match: " << percentRight << " : " << observations.returnPercentRight() << "\n";
-					}
-				}
-			}
-		}//end improv8 namespace
-
-
-	}//end mnist test namespace
-
-	//////////////////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////
-	//////////////////Start Allstate//////////////////////////////
-	if(doesExist("res/allstate/forest.csv") && true){//begin allstate test namespace
-		const std::string forestFileName3 = "res/allstate/forest.csv";
-		const std::string testFileName3 = "res/allstate/testObservations.csv";
-		const std::string traversalFileName3 = "res/allstate/traversal.csv";
-		inferenceSamples travs(traversalFileName3);
-		inferenceSamples observations(testFileName3);
-
 
 		{//start improv8 namespace
 			//////improv8/////////
 			std::cout << "\n\n\n"; 
 			std::cout << "running improv8 Test"; 
-
-			std::cout << "MultiTree\n"; 
-			for(int numCores = 1; numCores <= maxCores; numCores *= 2){	
-				std::cout << "MultiTree, numCores=" << numCores << "\n"; 
-				int treesPerBin = 2048/numCores;
-				improv8 tester(forestFileName3,1,travs, treesPerBin, 3);
+			//	for(int numDepth = 0; numDepth <= maxDepth; numDepth+=5){	
+			for(int numBins = 1; numBins <= 2048; numBins *= 2){	
+				improv8 tester(forestFileName1,1,travs, numBins, numDepth);
 				tester.printForest();
 				printf("size of a node is %d\n",(int) sizeof(padNode));
 				printf("starting run\n");
+				int treesPerBin = 2048/numBins;
 
-
-				for(int runNum = 0; runNum < numTimes; ++runNum){
-					start_time = std::chrono::high_resolution_clock::now();
-					tester.makePredictionsMultiTree(observations,numCores);
-					stop_time = std::chrono::high_resolution_clock::now();
-					diffMilli = stop_time - start_time;
-					outfile << std::fixed << "experiment1, Bin+, Allstate, MultiTree, " << numCores << ", " << diffMilli.count()<< std::endl;
-					if (observations.returnPercentRight() != percentRight){
-						std::cout << "percentRight does not match: " << percentRight << " : " << observations.returnPercentRight() << "\n";
-						//outfile << "percentRight does not match: " << percentRight << " : " << observations.returnPercentRight() << "\n";
+				for(int numCores = 1; numCores <= 32; numCores *= 2){	
+					if(numBins >= numCores){
+						for(int runNum = 0; runNum < numTimes; ++runNum){
+							start_time = std::chrono::high_resolution_clock::now();
+							tester.makePredictionsMultiTree(observations,numCores);
+							stop_time = std::chrono::high_resolution_clock::now();
+							diffMilli = stop_time - start_time;
+							outfile << std::fixed << "experiment2, Bin+, Higgs, MultiTree, " << treesPerBin << ", " << numDepth << ", " << diffMilli.count()<< ", " << numCores <<std::endl;
+							std::cout << "percentRight does not match: " << percentRight << " : " << observations.returnPercentRight() << "\n";
+							std::cout << std::fixed << "experiment2, Bin+, Higgs, MultiTree, " << treesPerBin << ", " << numDepth << ", " << diffMilli.count()<< ", " << numBins << ", " << numCores << std::endl;
+						}
 					}
 				}
 			}
 		}//end improv8 namespace
 
 
-	}//end Allstate test namespace
+		}//end higgs test namespace
+
+
+		////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////
+
+		if (doesExist("res/mnist/forest.csv") && true){//begin mnist test namespace
+			const std::string forestFileName2 = "res/mnist/forest.csv";
+			const std::string testFileName2 = "res/mnist/testObservations.csv";
+			const std::string traversalFileName2 = "res/mnist/traversal.csv";
+
+			inferenceSamples travs(traversalFileName2);
+			inferenceSamples observations(testFileName2);
+
+
+			{//start improv8 namespace
+				//////improv8/////////
+				std::cout << "\n\n\n"; 
+				std::cout << "running improv8 Test"; 
+				//for(int numDepth = 0; numDepth <= maxDepth; numDepth+=5){	
+				for(int numBins = 1; numBins <= 2048; numBins *= 2){	
+					improv8 tester(forestFileName2,1,travs, numBins, numDepth);
+					tester.printForest();
+					printf("size of a node is %d\n",(int) sizeof(padNode));
+					printf("starting run\n");
+					int treesPerBin = 2048/numBins;
+
+					for(int numCores = 1; numCores <= 32; numCores *= 2){	
+						if(numBins >= numCores){
+							for(int runNum = 0; runNum < numTimes; ++runNum){
+								start_time = std::chrono::high_resolution_clock::now();
+								tester.makePredictionsMultiTree(observations,numCores);
+								stop_time = std::chrono::high_resolution_clock::now();
+								diffMilli = stop_time - start_time;
+								outfile << std::fixed << "experiment2, Bin+, MNIST, MultiTree, " << treesPerBin << ", " << numDepth << ", " << diffMilli.count()<< ", " << numCores << std::endl;
+								std::cout << "percentRight does not match: " << percentRight << " : " << observations.returnPercentRight() << "\n";
+								std::cout << std::fixed << "experiment2, Bin+, MNIST, MultiTree, " << treesPerBin << ", " << numDepth << ", " << diffMilli.count()<< ", " << numBins << ", " << numCores << std::endl;
+							}
+						}
+					}
+				}
+
+			}//end improv8 namespace
+
+
+			}//end mnist test namespace
+
+			//////////////////////////////////////////////////////////////
+			//////////////////////////////////////////////////////////////
+			//////////////////Start Allstate//////////////////////////////
+			if(doesExist("res/allstate/forest.csv") && true){//begin allstate test namespace
+				const std::string forestFileName1 = "res/allstate/forest.csv";
+				const std::string testFileName1 = "res/allstate/testObservations.csv";
+				const std::string traversalFileName1 = "res/allstate/traversal.csv";
+				inferenceSamples travs(traversalFileName1);
+				inferenceSamples observations(testFileName1);
+
+
+				{//start improv8 namespace
+					//////improv8/////////
+					std::cout << "\n\n\n"; 
+					std::cout << "running improv8 Test"; 
+
+					//	for(int numDepth = 0; numDepth <= maxDepth; numDepth+=5){	
+					for(int numBins = 1; numBins <= 2048; numBins *= 2){	
+						improv8 tester(forestFileName1,1,travs, numBins, numDepth);
+						tester.printForest();
+						printf("size of a node is %d\n",(int) sizeof(padNode));
+						printf("starting run\n");
+						int treesPerBin = 2048/numBins;
+
+						for(int numCores = 1; numCores <= 32; numCores *= 2){	
+							if(numBins >= numCores){
+								for(int runNum = 0; runNum < numTimes; ++runNum){
+									start_time = std::chrono::high_resolution_clock::now();
+									tester.makePredictionsMultiTree(observations,numCores);
+									stop_time = std::chrono::high_resolution_clock::now();
+									diffMilli = stop_time - start_time;
+									outfile << std::fixed << "experiment2, Bin+, Allstate, MultiTree, " << treesPerBin << ", " << numDepth << ", " << diffMilli.count()<< ", " << numCores << std::endl;
+									std::cout << "percentRight does not match: " << percentRight << " : " << observations.returnPercentRight() << "\n";
+									std::cout << std::fixed << "experiment2, Bin+, Allstate, MultiTree, " << treesPerBin << ", " << numDepth << ", " << diffMilli.count()<< ", " << numBins << ", " << numCores << std::endl;
+								}
+							}
+						}
+					}
+
+				}//end improv8 namespace
+
+
+				}//end Allstate test namespace
 
 
 
-	outfile.close();
-	return 0;
-}
+				outfile.close();
+				return 0;
+			}
